27d0132a3d9f3d7bf22448b2a07752a71dfe2301
John F Incantalupo <john.incantalupo@gmail.com>
2013-02-28 21:15:19 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collegerise::Application.config.secret_token = 'c3d69b2ca6f981159b02c9f315bacfc7374ab77bf17e4d094d964e6020f2bd67ec73e614540c9a45ad6c95c9ea049ed9ca09ad6d0d00b136839a927c233a9142'
